9aa3d35a8917032526498a655ddfe517d8e057fb
Hamza <hamza.elmahjour@gmail.com>
2023-10-14 16:22:07 +0100

    I want to create a left side where a carrousel of cards show positve comments during submission

---
+++ js/test_comments.js
@@ -0,0 +1,54 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
+}
+
+.comment-section {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20px;
+}
+
+.comment-form {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    width: 300px;
+    margin-right: 20px;
+}
+
+.comment-form h2 {
+    font-size: 18px;
+}
+
+textarea,
+input {
+    width: 100%;
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+#submitBtn {
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.comment-carousel {
+    width: 300px;
+}
+
+.comment-cards {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
